28dbd48d98a150610ebcfe412f45e72bbfdc4823
Scherezade Momin <scherezade.momin@gmail.com>
2012-12-16 12:30:03 -0500

    Initial commit

---
+++ fashion_scrape/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FashionScrape::Application.config.secret_token = '422c8c4371e17e5eb5dae28cf74f95fc51212c2df993fbdb9fcf6fd2098d764f430b06a70f6560a7b8981040f3b0a79cc6347b6e5f04f749d70ef6e2fccd53b5'
